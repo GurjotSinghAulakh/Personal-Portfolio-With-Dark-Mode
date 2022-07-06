f10e396ba64fb80196cd0fe410329e0e07beb6f1
Gurjot Singh Aulakh <s351873@oslomet.no>
2022-07-06 18:47:54 +0200

    Added necessary files to the project
    - Fonts
    - Images
    - Added Navbar component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/*import logo from './logo.svg';*/
+import './App.css';
+
+import Navbar from './components/Navbar/Navbar'
+import Intro from './components/Intro/Intro'
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar/>
+    <Intro/>
+    </div>
+  );
+}
+
+export default App;
