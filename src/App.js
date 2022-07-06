54caaf450080366e0035bf86623496273906d044
Gurjot Singh Aulakh <s351873@oslomet.no>
2022-07-06 20:16:13 +0200

    Added some new components
    Components which is added:
    
    - Floating div for emojis and some text
    - Intro component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Navbar from './components/Navbar/Navbar'
 import Intro from './components/Intro/Intro'
+import Services from './components/Services/Services';
 
 function App() {
   return (
     <div className="App">
-    <Navbar/>
-    <Intro/>
+      <Navbar/>
+      <Intro/>
+      <Services/>
     </div>
   );
 }
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Intro.css'
+import FloatingDiv from '../FloatingDiv/FloatingDiv'
 
 import Github from '../../img/github.png'
 import LinkedIn from '../../img/linkedin.png'
@@ -7,6 +8,12 @@ import Instagram from '../../img/instagram.png'
 
 import Vector1 from '../../img/Vector1.png'
 import Vector2 from '../../img/Vector2.png'
+
+import Boy from '../../img/boy.png';
+import Thrumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import Glassesemoji from '../../img/glassesemoji.png';
+
 
 const Intro = () => {
   return (
@@ -30,7 +37,36 @@ const Intro = () => {
 
         <div className="i-right">
 
-            I am right side
+            <img src={Vector1} alt="" />
+            <img src={Vector2} alt="" />
+            <img src={Boy} alt="" />
+            <img src={Glassesemoji} alt="" />
+
+            <div style={{top: '-4%', left: '58%'}}>
+                <FloatingDiv image={Crown} txt1='Web' txt2='Developer'/>
+            </div>
+
+            <div style={{
+                top: '18rem', 
+                left: '3rem'
+            }}>
+                <FloatingDiv image={Thrumbup} txt1='Best' txt2='Design'/>      
+            </div>
+
+            {/* Blur divs */}
+            <div className='blur' style={{
+                background: 'rgb(238 210 255)'
+            }}></div>
+
+            <div className='blur' style={{
+                background: '#C1F5FF',
+                top: '17rem',
+                width: '21rem',
+                height: '11rem',
+                left: '-9rem'
+            }}></div>
+
+
         </div>
     </div>
   )
