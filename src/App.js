3006b4d5d8d6194e6ed31b6db5af1d15854eb118
Gurjot Singh Aulakh <s351873@oslomet.no>
2022-07-09 13:57:29 +0200

    Added a new component
    - Added a contact me component
    - And contacted it to my email using emailjs og useState, so that I get their messages on my email
    - Styled the contact me page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Experience from './components/Experience/Experience';
 import Works from './components/Works/Works';
 import Portfolio from './components/Portfolio/Portfolio';
 import Testimonials from './components/Testimonials/Testimonials';
+import Contact from './components/Contact/Contact';
 
 import { useEffect, useState } from 'react'
 
@@ -32,6 +33,7 @@ function App() {
       <Works/>
       <Portfolio/>
       <Testimonials/>
+      <Contact/>
     </div>
   );
 }
